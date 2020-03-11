8981fe283181f07ec1703fc4c447931a28695061
linuxd234 <46240496@qq.com>
2020-03-11 17:38:18 +0800

    统计字符

---
+++ tongjizifu.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+int count[10] = {0}; //全部初化为零
+int main()
+{
+    int input[64] = {0}; //必须初始化，变量不初始化内容随机
+    int charSum(int);
+    printf("请输入int数组，以0结束以enter、空格、tab作为分隔。\n");
+    //printf("�������飡\n");
+    int num;
+    int i = 0;
+    scanf("%d", &num);
+    while (num != 0)
+    {
+        input[i] = num;
+        scanf("%d", &num);
+        i++;
+    }
+    printf("\n--------------------------------------------\n");
+    int j=0;
+     while (input[j]!=0)
+         {
+             int temp=input[j];
+              while(temp!=0){
+                  int c=temp%10;//获得末尾数
+                  charSum(c);
+                  temp=temp/10;//截取前一位
+              }
+              j++;
+         }
+
+        for (int i = 0; i < 10; i++)
+        {
+            if(count[i]!=0){
+                printf("%d:%d;\n",i,count[i]);
+            }            
+        }
+        
+
+    return 0;
+}
+
+int charSum(int c)
+{
+    switch (c)
+    {
+    case 0:
+        count[0]++;
+        break;
+
+    case 1:
+        count[1]++;
+        break;
+    case 2:
+        count[2]++;
+        break;
+    case 3:
+        count[3]++;
+        break;
+    case 4:
+        count[4]++;
+        break;
+    case 5:
+        count[5]++;
+        break;
+    case 6:
+        count[6]++;
+        break;
+    case 7:
+        count[7]++;
+        break;
+    case 8:
+        count[8]++;
+        break;
+    case 9:
+        count[9]++;
+        break;
+    }
+    return 0;
+}
